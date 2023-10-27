c0e9a1ec5207236fff57bdca688829f7bc0d10dc
Mohd Ahtsham <ahtsham4698@gmail.com>
2023-10-27 17:56:09 +0530

    updated

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".click-me").click(function () {
+    $(".clicked").toggle();
+  });
+});
+
+
+$(document).ready(function () {
+  $(".humburgrr").click(function () {
+    $(".main-header-one").toggle();
+  });
+});
+
+
